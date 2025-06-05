fa3dc06d92e88a56a0c34c6fc3757ce581b87f30
DevloperAmanSingh <amansinghneh211205@gmail.com>
2025-06-05 17:18:21 +0530

    Add configuration file for HLS transcoder project, loading environment variables for AWS region, S3 bucket names, and SQS queue URL to facilitate cloud integration.

---
+++ hls-transcoder/src/config.ts
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+export const config = {
+  region: process.env.AWS_REGION,
+  s3Bucket: process.env.S3_BUCKET_NAME,
+  queueUrl: process.env.SQS_QUEUE_URL,
+  s3BucketOutput: process.env.S3_BUCKET_OUTPUT,
+};
